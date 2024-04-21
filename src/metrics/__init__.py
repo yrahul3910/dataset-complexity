796d3bae6188302f9caa4779561ff9f3dade6b0c
Dref360 <frederic.branchaud.charron@gmail.com>
2024-04-21 13:43:03 -0400

    Add DCoL metrics to init file

--- src/metrics/__init__.py
+++ src/metrics/__init__.py
@@ -1,5 +1,6 @@
 from metrics.base import BaseMetric
 from metrics.csg import CSGMetric
+from metrics.dcol import F1, F2, F3, F4, L1, L2, L3, N1, N2, N3, N4, T1, T2, F1v
 from metrics.smoothness import SmoothnessMetric
 from metrics.strong_convexity import StrongConvexityMetric
 from metrics.types import ArrayLike, Float, NDArray, NpFloat
@@ -14,4 +15,18 @@ __all__ = [
     "NpFloat",
     "SmoothnessMetric",
     "StrongConvexityMetric",
+    "F1",
+    "F1v",
+    "F2",
+    "F3",
+    "F4",
+    "L1",
+    "L2",
+    "L3",
+    "N1",
+    "N2",
+    "N3",
+    "N4",
+    "T1",
+    "T2"
 ]
