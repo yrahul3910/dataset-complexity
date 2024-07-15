dbe710895d7f84c3564751cc75411810c3550822
Rahul Yedida <rahul.yedida@pm.me>
2024-07-14 20:52:19 -0400

    Bug fixes

--- src/metrics/csg.py
+++ src/metrics/csg.py
@@ -7,6 +7,7 @@ from spectral_metric.estimator import CumulativeGradientEstimator
 class CSGMetric(BaseMetric):
     def __init__(self, x_train: NDArray[NpFloat], y_train: ArrayLike):
         super().__init__(x_train, y_train)
+        self.__name__ = "CSG"
         self.estimator = CumulativeGradientEstimator(M_sample=250, k_nearest=5)
 
     def get_complexity(self) -> Float:
