19e389492c88c8dbb791e5858b9418468fb767e9
Rahul Yedida <rahul.yedida@pm.me>
2024-07-29 01:01:04 -0400

    Bug fixes, added full results

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
+import gc
+
 from data.bugzilla_issue import bugzilla_loader
 from data.defect import defect_loader
 from data.github_issue import github_issue_loader
 from data.static_code import static_code_loader
 from data.uci import uci_loader
+
+import numpy as np
 
 from loguru import logger
 from metrics import get_all_estimators
@@ -16,9 +20,17 @@ def iterator():
 
 
 logger.add("logs.txt")
+lines = open("logs.txt", "r").read()
 for name, dataset in iterator():
     for metric in get_all_estimators():
         estimator = metric(dataset.x_train, dataset.y_train)
+        print(f"Processing {name} - {estimator.__name__}")
+        if f"'dataset': '{name}', 'metric': '{estimator.__name__}'" in lines:
+            print(f"Skipping {name} - {estimator.__name__}")
+            continue
+
+        dataset.x_train = np.array(dataset.x_train)
+        dataset.y_train = np.array(dataset.y_train)
         complexity = estimator.get_complexity()
 
         # Log as JSONL
@@ -29,3 +41,5 @@ for name, dataset in iterator():
                 "complexity": complexity,
             }
         )
+
+        gc.collect()
